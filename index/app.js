6c7fd93e329a44577b039df673cec136cd86c15d
anton202 <a.kluge202@gmail.com>
2018-06-17 11:50:37 +0300

    restructres app.js in index

--- index/app.js
+++ index/app.js
@@ -1,49 +1,71 @@
+const app = (function () {
+    const openGamesContainer = document.querySelector('.openGamesBtn');
+    const socket = io();
 
-const openGamesContainer = document.querySelector('.openGamesBtn');
-const socket = io();
+    const joinGame = function () {
+        openGamesContainer.addEventListener('click', (e) => {
 
+            if (e.target.className === 'join') {
+                fetch('http://localhost:8000/setJoinGameRoomName', {
+                    method: 'POST', body: JSON.stringify({ roomName: e.target.textContent }), headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'same-origin'
+                })
+                location.href = '../joinGame/index.html'
 
-fetch('http://localhost:8000/isSession',{credentials:'same-origin'})
-.then(()=>{});
-
-openGamesContainer.addEventListener('click', (e) => {
-    
-    if (e.target.className === 'join') {
-        fetch('http://localhost:8000/setJoinGameRoomName', {
-            method: 'POST', body: JSON.stringify({roomName:e.target.textContent}), headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'same-origin'
+            }
         })
-        location.href = '../joinGame/index.html'
-      
     }
-})
+
+    const ioEvents = function () {
+        socket.on('openRooms', (rooms) => {
+            rooms.forEach(room => {
+                joinRoom(room)
+            })
+        })
+
+        socket.on('newRoomCreated', (room) => {
+            joinRoom(room);
+        })
+
+        socket.on('deleteRoom', (roomName) => {
+            let rooms = document.querySelectorAll('button');
+            rooms.forEach((room) => {
+                if (room.textContent === roomName) {
+                    openGamesContainer.removeChild(room);
+                }
+            })
+        })
+
+    }
+
+    const joinRoom = function (room) {
+        let joinRoomBt = document.createElement('button');
+        joinRoomBt.textContent = room;
+        joinRoomBt.className = 'join';
+        joinRoomBt.style.display = 'block';
+        openGamesContainer.appendChild(joinRoomBt)
+    }
 
 
-socket.on('openRooms', (rooms) => {
-    rooms.forEach(room => {
-       joinRoom(room)
-    })
-})
-
-socket.on('newRoomCreated',(room)=>{
-    joinRoom(room);
-})
-
-socket.on('deleteRoom',(roomName)=>{
-    let rooms = document.querySelectorAll('button');
-    rooms.forEach((room)=>{
-        if(room.textContent === roomName){
-            openGamesContainer.removeChild(room);
+    return {
+        init: function () {
+            fetch('http://localhost:8000/isSession', { credentials: 'same-origin' })
+                .then(() => { });
+            joinGame();
+            ioEvents();
         }
-    })
-})
-
-function joinRoom(room){
-    let joinRoomBt = document.createElement('button');
-    joinRoomBt.textContent = room;
-    joinRoomBt.className = 'join';
-    joinRoomBt.style.display = 'block';
-    openGamesContainer.appendChild(joinRoomBt)
+    }
 }
+    ())
+
+app.init();
+
+
+
+
+
+
+
+
